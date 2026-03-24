bd2818ba438d531e18f66dd700f5b924c30ea5ef
Marcio Bueno <marciobueno@gmail.com>
2026-03-24 19:59:13 -0300

    transferida a lógica da página de tarefas do NextJS do semestre passado
    Adicionado o pacote TanStack Query

--- expo-01/app/index.tsx
+++ expo-01/app/index.tsx
@@ -1,18 +1,20 @@
 import { Text, View } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        flexDirection: "row",
         justifyContent: "center",
         alignItems: "center",
       }}
     >
       <Text>Olá, Turma!</Text>
-      <View style={{ height: 100, backgroundColor: "blue", flex: 0.1 }} />
-      <View style={{ height: 100, backgroundColor: "red", flex: 0.4 }} />
+      <View style={{ width: 100, backgroundColor: "blue", flex: 0.1 }} />
+      <View style={{ width: 100, backgroundColor: "red", flex: 0.4 }} />
+      <Link href="/about">About</Link>
+      <Link href="/tarefas">Tarefas</Link>
     </View>
   );
 }
---
+++ expo-01/back4app/index.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+export async function getTarefas() {
+  const response = await axios.get(
+    "https://parseapi.back4app.com/classes/Tarefa",
+    {
+      headers: {
+        "X-Parse-Application-Id": "jnYc2snzzFZes5yr1TnS2l0ny7nB1Fyvlj8bq9wZ",
+        "X-Parse-JavaScript-Key": "iDYRd5O3Z2SnyfafWPneaqHiztKFSxFISMGqoBIY",
+      },
+    },
+  );
+  return response.data.results;
+}
+
+export async function adicionarTarefa(novaTarefa) {
+  const response = await axios.post(
+    "https://parseapi.back4app.com/classes/Tarefa",
+    novaTarefa,
+    {
+      headers: {
+        "X-Parse-Application-Id": "jnYc2snzzFZes5yr1TnS2l0ny7nB1Fyvlj8bq9wZ",
+        "X-Parse-JavaScript-Key": "iDYRd5O3Z2SnyfafWPneaqHiztKFSxFISMGqoBIY",
+        "Content-Type": "application/json",
+      },
+    },
+  );
+  return response.data;
+}
